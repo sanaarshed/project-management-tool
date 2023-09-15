8b073a16005bbf315634dd37dcf0484aa5cc9cb4
ameerhamzabutt555 <ameerhamzabutt555@gmail.com>
2023-09-15 17:16:12 +0500

    download file

---
+++ server/db/migrations/20230915102228-create-invitation-table.js
@@ -0,0 +1,51 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Invitations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      team_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Teams",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      // Add more columns as needed (e.g., file_path, file_size, file_type, etc.)
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Invitations");
+  },
+};
